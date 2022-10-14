0c0b950b716f1d0e65f96bf887619d7ae8e37393
Valentine Yurchenko <45362610+VelHRH@users.noreply.github.com>
2022-10-14 17:43:51 +0300

    Heading created

---
+++ components/Heading.js
@@ -0,0 +1,5 @@
+const Heading = ({text}) => {
+  return <h1 className="text-2xl md:text-3xl text-center">{text}</h1>
+}
+
+export default Heading;
---
+++ pages/create-queue.js
@@ -0,0 +1,8 @@
+import Head from 'next/head'
+import Heading from '../components/Heading';
+
+const CreateQueue = () => (
+  <Heading text="Створення нової черги" />
+);
+
+export default CreateQueue;
