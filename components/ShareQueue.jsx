3286ed28a71c068479debd659173348f5fa4305c
Valentine Yurchenko <45362610+VelHRH@users.noreply.github.com>
2022-10-17 15:54:43 +0300

    Share queue component

---
+++ components/ShareQueue.jsx
@@ -0,0 +1,11 @@
+import { ShareIcon } from "@heroicons/react/outline";
+
+const ShareQueue = () => {
+ return (
+  <button className="ml-10 p-2 rounded-md hover:bg-slate-900 hover:text-slate-50 transition">
+   <ShareIcon className="w-5 md:w-7" />
+  </button>
+ );
+};
+
+export default ShareQueue;
