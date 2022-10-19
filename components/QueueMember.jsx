adae916169fa210c0e264d95d51540bf27f6bfa1
advanced-user <stolbetskiy.yura@gmail.com>
2022-10-19 20:54:54 +0200

    Queue cards functionality

--- components/QueueMember.jsx
+++ components/QueueMember.jsx
@@ -1,97 +1,92 @@
-import { XIcon, CheckIcon, PlusIcon } from "@heroicons/react/outline";
+import { XIcon, PlusIcon } from "@heroicons/react/outline";
+import Button from "./Button";
 
-const QueueMember = ({ number, name, work, variant }) => {
- if (variant === "stranger-out") {
-  return (
-   <div className="w-full bg-slate-400 h-[100px] rounded-xl mb-3">
-    <div className="flex justify-between px-5 py-3 font-semibold">
-     <h1 className="text-2xl">
-      {number} - {name}
-     </h1>
-     <h2 className="text-xl">{work}</h2>
-    </div>
-   </div>
-  );
- }
-
- if (variant === "stranger-in") {
-  return (
-   <div className="w-full bg-slate-400 h-[100px] rounded-xl mb-3">
-    <div className="flex justify-between px-5 py-3 font-semibold">
-     <h1 className="text-2xl">
-      {number} - {name}
-     </h1>
-     <div className="flex">
-      <h2 className="text-xl">{work}</h2>
-      <button className="bg-slate-300 rounded-md px-1.5 ml-3 mt-[-5px]">
-       <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        className="w-6 h-6"
-       >
-        <path
-         strokeLinecap="round"
-         strokeLinejoin="round"
-         d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
-        />
-       </svg>
-      </button>
+const NotInQueue = ({ number, name, work }) => {
+ return (
+     <div className="w-full bg-slate-400 h-[100px] rounded-xl mb-3">
+      <div className="flex justify-between px-5 py-3 font-semibold">
+       <h1 className="text-2xl">
+        {number} - {name}
+       </h1>
+       <h2 className="text-xl">{work}</h2>
+      </div>
      </div>
-    </div>
-   </div>
-  );
- }
-
- if (variant === "me") {
-  return (
-   <div className="w-full bg-slate-400 h-[100px] rounded-xl mb-3 border-4 border-solid border-yellow-400">
-    <div className="flex justify-between px-5 py-3 font-semibold">
-     <h1 className="text-2xl">
-      {number} - {name}
-     </h1>
-     <div className="flex">
-      <h2 className="text-xl">{work}</h2>
-      <button className="bg-slate-300 rounded-md px-1.5 ml-3 mt-[-5px]">
-       <XIcon className="w-6" />
-      </button>
+ );
+}
+const SwapPlaces = ({ id, number, name, work, callback }) => {
+ return (
+     <div className="w-full bg-slate-400 h-[100px] rounded-xl mb-3">
+      <div className="flex justify-between px-5 py-3 font-semibold">
+       <h1 className="text-2xl">
+        {number} - {name}
+       </h1>
+       <div className="flex">
+        <h2 className="text-xl">{work}</h2>
+        <button onClick={() => callback(id, name)} className="bg-slate-300 rounded-md px-1.5 ml-3 mt-[-5px]">
+         <svg
+             xmlns="http://www.w3.org/2000/svg"
+             fill="none"
+             viewBox="0 0 24 24"
+             strokeWidth={1.5}
+             stroke="currentColor"
+             className="w-6 h-6"
+         >
+          <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
+          />
+         </svg>
+        </button>
+       </div>
+      </div>
      </div>
-    </div>
-   </div>
-  );
- }
+ );
+}
 
- if (variant === "empty") {
-  return (
-   <div className="w-full cursor-pointer bg-slate-300 h-[100px] rounded-xl mb-3 hover:bg-slate-400 transition">
-    <div className="flex justify-between px-5 py-3 font-semibold">
-     <h1 className="text-2xl">
-      {number} - <u>Вільне місце</u>
-     </h1>
-     <PlusIcon className="w-6" />
-    </div>
-   </div>
-  );
- }
-
- if (variant === "entry") {
-  return (
-   <div className="w-full bg-amber-200 h-[100px] rounded-xl mb-3">
-    <div className="flex flex-col items-center px-5 py-3 font-semibold">
-     <h1 className="text-2xl">Зайняти це місце?</h1>
-     <div className="flex">
-      <button className="m-3 px-10 py-1 rounded-lg bg-green-300 transition hover:border-2 hover:border-green-400">
-       <CheckIcon className="w-6" />
-      </button>
-      <button className="m-3 px-10 py-1 rounded-lg bg-red-300 transition hover:border-2 hover:border-red-500">
-       <XIcon className="w-6" />
-      </button>
+const Me = ({ id, number, name, work, callback }) => {
+ return (
+     <div className="w-full bg-slate-400 h-[100px] rounded-xl mb-3 border-4 border-solid border-yellow-400">
+      <div className="flex justify-between px-5 py-3 font-semibold">
+       <h1 className="text-2xl">
+        {number} - {name}
+       </h1>
+       <div className="flex">
+        <h2 className="text-xl">{work}</h2>
+        <button onClick={() => callback(id)} className="bg-slate-300 rounded-md px-1.5 ml-3 mt-[-5px]">
+         <XIcon className="w-6" />
+        </button>
+       </div>
+      </div>
      </div>
-    </div>
-   </div>
+ );
+}
+
+const EmptyPlace = ({ id, number, callback }) => {
+  return (
+    <Button onClick={() => callback(id, number)}>
+      <div className="w-full cursor-pointer bg-slate-300 h-[100px] rounded-xl mb-3 hover:bg-slate-400 transition">
+        <div className="flex justify-between px-5 py-3 font-semibold">
+          <h1 className="text-2xl">
+            {number} - <u>Вільне місце</u>
+          </h1>
+          <PlusIcon className="w-6"/>
+        </div>
+      </div>
+    </Button>
   );
+}
+
+const QueueMember = ({ id, number, name, work, variant, callback }) => {
+ switch (variant) {
+  case "stranger-out":
+   return <NotInQueue number={number} name={name} work={work} />
+  case "stranger-in":
+   return <SwapPlaces id={id} number={number} name={name} work={work} callback={callback} />
+  case "me":
+   return <Me id={id} number={number} name={name} work={work} callback={callback} />
+  case "empty":
+   return <EmptyPlace id={id} number={number} callback={callback} />
  }
 };
 
--- components/QueueMembers.jsx
+++ components/QueueMembers.jsx
@@ -1,21 +1,62 @@
 import QueueMember from "../components/QueueMember";
+import {ConfirmQueueMember} from "./ConfirmQueueMember";
+import {useState} from "react";
 
-export const QueueMembers = () => (
- <>
-  <QueueMember
-   variant="stranger-out"
-   number="#1"
-   name="Спектров Денис"
-   work="ПЗ2"
-  />
-  <QueueMember
-   variant="stranger-in"
-   number="#2"
-   name="Спектров Денис"
-   work="ПЗ2"
-  />
-  <QueueMember variant="empty" number="#3" />
-  <QueueMember variant="me" number="#4" name="Спектров Денис" work="ПЗ2" />
-  <QueueMember variant="entry" />
- </>
-);
+export const QueueMembers = () => {
+  const TakePlace = (id, placeNumber) => {
+    setIsConfirmActive(true)
+    setConfirmTitle(confirmTitle = `Зайняти місце ${placeNumber} ?`)
+  }
+
+  const SwapPlaces = (id, name) => {
+    setIsConfirmActive(true)
+    setConfirmTitle(confirmTitle = `Помінятись місцями з ${name} ?`)
+  }
+
+  const FreePlace = (id) => {
+    setIsConfirmActive(true)
+    setConfirmTitle(confirmTitle = "Залишити чергу?")
+  }
+
+  const Confirm = () => {
+    setConfirmTitle("Okay")
+  }
+
+  const Cancel = () => {
+    setIsConfirmActive(false)
+  }
+
+  let [isConfirmActive, setIsConfirmActive] = useState(false);
+  let [confirmTitle, setConfirmTitle] = useState("");
+
+  let members = [{
+    id: 0, variant: "stranger-out", number: "#1", name: "Спектров Денис", work: "ПЗ2",
+  }, {
+    id: 1, variant: "stranger-in", number: "#2", name: "Спектров Денис", work: "ПЗ2", callback: SwapPlaces
+  }, {
+    id: 2, variant: "empty", number: "#3", callback: TakePlace
+  }, {
+    id: 3, variant: "me", number: "#4", name: "Спектров Денис", work: "ПЗ2", callback: FreePlace
+  }]
+
+  let membersComp = members.map(({
+                                   id,
+                                   variant,
+                                   number,
+                                   name,
+                                   work,
+                                   callback
+                                 }) => (// eslint-disable-next-line react/jsx-key
+    <QueueMember id={id} variant={variant} number={number} name={name} work={work} callback={callback}/>))
+
+  if (!isConfirmActive) {
+    return <>
+      {membersComp}
+    </>
+  }
+
+  return <>
+    {membersComp}
+    <ConfirmQueueMember confirmCallback={Confirm} cancelCallback={Cancel} title={confirmTitle}/>
+  </>
+};
