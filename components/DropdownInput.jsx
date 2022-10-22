9267fd5e98d861096e0337a8a56129cec8e3b9fc
Sergei Kalugin <kalugin.serej@gmail.com>
2022-10-22 17:55:07 +0300

    refactor

--- components/DropdownInput.jsx
+++ components/DropdownInput.jsx
@@ -1,30 +1,26 @@
 import { useState } from "react";
 import { Input } from "./Input";
+import {ChevronDownIcon} from "@heroicons/react/outline";
 
-export const DropdownInput = ({ icon, title }) => {
+export const DropdownInput = ({ title }) => {
  const [outDropdown, setOutDropdown] = useState("безобмежна");
  const [open, setOpen] = useState(false);
 
  const toggle = () => setOpen(!open);
 
  const handleInput = (e) =>
-  e.target.value == ""
+  e.target.value === ""
    ? setOutDropdown("безобмежна")
    : setOutDropdown(e.target.value);
 
  return (
   <div className="w-10/12 lg:w-1/3">
-   <div
-    className="flex justify-between mb-2 cursor-pointer"
-    onClick={() => {
-     toggle();
-    }}
-   >
+   <div className="flex justify-between mb-2 cursor-pointer" onClick={toggle} >
     <div>
      {title}
      <b>{outDropdown}</b>
     </div>
-    {icon}
+    <ChevronDownIcon className='w-5' />
    </div>
    <div className="absolute w-10/12 lg:w-1/3">
     {open && <Input handleInput={handleInput} placeholder="Кількість" />}
