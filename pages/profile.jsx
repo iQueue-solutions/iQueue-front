b7f8f98b7fb276546626451df85ff85a113d1540
Valentine Yurchenko <45362610+VelHRH@users.noreply.github.com>
2022-10-26 23:32:04 +0300

    Profile list styling

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -1,33 +1,40 @@
-import {Heading} from "../components/Heading";
-import {API_URL} from "../constants";
-import {CheckIcon} from "@heroicons/react/outline";
+import { Heading } from "../components/Heading";
+import { API_URL } from "../constants";
+import { CheckIcon } from "@heroicons/react/outline";
 
 export async function getServerSideProps() {
-  const response = await fetch(`${API_URL}/Users`);
-  const users = await response.json();
+ const response = await fetch(`${API_URL}/Users`);
+ const users = await response.json();
 
-  return {
-    props: {
-      users: users
-    },
-  };
+ return {
+  props: {
+   users: users,
+  },
+ };
 }
 
-const Profile = ({users}) => {
-  console.log(users);
-  return (
-    <div>
-      <Heading>Profile</Heading>
-      <h1>Profile</h1>
-      {users.map((user) => (
-        <div key={user.id}>
-          <button><CheckIcon /> check</button>
-          {user.firstName} {user.lastName}
-          {user.email && <> ({user.email}) </> }
-        </div>
-      ))}
-    </div>
-  );
-}
+const Profile = ({ users }) => {
+ console.log(users);
+ return (
+  <div className="flex flex-col items-center">
+   <h1 className="text-4xl md:text-8xl md:lowercase font-semibold md:font-light text-purple-800 mb-9">
+    Профіль
+   </h1>
+   <div className="flex flex-col items-start">
+    {users.map((user) => (
+     <div key={user.id} className="mb-2 text-xl font-semibold flex">
+      <button className="p-2 bg-purple-800 text-slate-50 border-2 border-purple-800 hover:bg-slate-50 hover:text-purple-800 transition mr-1">
+       <CheckIcon className="w-6" />
+      </button>
+      <div className="mt-2">
+       {user.firstName} {user.lastName}
+       {user.email && <> ({user.email}) </>}
+      </div>
+     </div>
+    ))}
+   </div>
+  </div>
+ );
+};
 
 export default Profile;
