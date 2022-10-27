16fc82d39aedc81aa73334cd62cb2c38450534e1
stolbetskiy.yura@gmail.com <kalugin.serej@gmail.com>
2022-10-27 03:59:49 +0300

    refactor dropdown api

--- pages/queue/new.jsx
+++ pages/queue/new.jsx
@@ -3,17 +3,21 @@ import {Button} from '../../components/Button';
 import { QueueCreationForm} from '../../components/QueueCreationForm';
 import {DropdownInput} from '../../components/DropdownInput'
 import {ChevronRightIcon, ArrowSmLeftIcon} from "@heroicons/react/outline";
+import {useState} from "react";
 
-const New = () => (
+const New = () => {
+  const [maxPeople, setMaxPeople] = useState(0);
 
+  return (
     <div className='w-full flex flex-col md:flex-row items-center justify-center'>
-    <CreateQueueHeading icon={<ArrowSmLeftIcon className='w-8' />}>Нова черга</CreateQueueHeading>
-    <div className='flex flex-col w-10/12 md:w-1/3'>
-    <QueueCreationForm />
-    <DropdownInput title="Кількість учасників - " />
-    <Button icon={<ChevronRightIcon className='w-5 md:w-6' />} color="purple" variant="solid" margin={10}>Створити чергу</Button>
+      <CreateQueueHeading icon={<ArrowSmLeftIcon className='w-8' />}>Нова черга</CreateQueueHeading>
+      <div className='flex flex-col w-10/12 md:w-1/3'>
+        <QueueCreationForm />
+        <DropdownInput title="Кількість учасників - " amount={maxPeople} setAmount={setMaxPeople} />
+        <Button icon={<ChevronRightIcon className='w-5 md:w-6' />} color="purple" variant="solid" margin={10}>Створити чергу</Button>
+      </div>
     </div>
-    </div>
-);
+  )
+}
 
 export default New;
