286179c4012a6855b0c88dec109968737742386d
Sergei Kalugin <kalugin.serej@gmail.com>
2022-10-30 01:16:48 +0300

    display queue info except records

---
+++ pages/404.js
@@ -0,0 +1,8 @@
+import {Heading} from "../components/Heading";
+
+export default function Custom404() {
+  return (<>
+    <Heading>404</Heading>
+    <p>Такої сторінки не існує</p>
+  </>);
+}
--- pages/queue/[id].jsx
+++ pages/queue/[id].jsx
@@ -1,15 +1,44 @@
 import { QueueInfo } from "../../components/QueueInfo";
 import { QueueMembers } from "../../components/QueueMembers";
 import { CreateQueueHeading } from "../../components/CreateQueueHeading";
+import {API_URL} from "../../constants";
+import {DateToQueueDate} from "../../utlis";
 
-const Id = () => {
+export const getServerSideProps = async (ctx) => {
+
+  const queueResponse = await fetch(`${API_URL}/queues/${ctx.query.id}`);
+
+  if (!queueResponse.ok) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const queue = await queueResponse.json();
+
+  const adminResponse = await fetch(`${API_URL}/users/${queue.adminId}`);
+  const admin = await adminResponse.json();
+
+  return {
+    props: {
+      name: queue.name,
+      queueInfo: {
+        start: DateToQueueDate( new Date(queue.openTime) ),
+        end: DateToQueueDate( new Date(queue.closeTime) ),
+        creator: `${admin.lastName} ${admin.firstName}`,
+      },
+    }
+  }
+}
+
+const Id = ({name, queueInfo}) => {
   return (
     <div className="flex justify-center">
       <div className="flex flex-col w-11/12 items-center">
-        <CreateQueueHeading>ПарП ПЗПІ-20-2</CreateQueueHeading>
+        <CreateQueueHeading>{name}</CreateQueueHeading>
         <div className="md:flex w-full mt-5 hidden">
           <div className="basis-1/5 px-10">
-            <QueueInfo creator="Олійник О.В." start="22.10.2022" end="29.10.2022" />
+            <QueueInfo {...queueInfo} />
           </div>
           <div className="basis-3/5">
             <QueueMembers />
