cc96472e7f0b758a38f20cdfb7cc1b2c9c003aae
Valentine Yurchenko <45362610+VelHRH@users.noreply.github.com>
2022-11-11 15:50:52 +0300

    Add list aligned to left

--- pages/404.js
+++ pages/404.js
@@ -1,8 +1,10 @@
+import { AddToQueue } from "../components/AddToQueue";
 import {Heading} from "../components/Heading";
 
 export default function Custom404() {
   return (<>
     <Heading>404</Heading>
+    <AddToQueue />
     <p className="text-center text-3xl font-semibold">Такої сторінки не існує</p>
   </>);
 }
