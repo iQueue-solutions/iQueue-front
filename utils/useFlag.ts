ef7fd9cd2c757566a4410f2b32625e63994e139e
Serhii Kaluhin <serhii.kaluhin@nure.ua>
2022-11-17 13:52:22 +0200

    create useFlag hook

--- utils/useFlag.ts
+++ utils/useFlag.ts
@@ -4,7 +4,7 @@ export const useFlag = (initialValue: boolean = false) => {
   const [flag, setFlag] = useState(initialValue);
   const setTrue = () => setFlag(true);
   const setFalse = () => setFlag(false);
-  const toggle = () => setFlag(!flag);
+  const toggle = () => setFlag(f => !f);
 
   return [flag, setTrue, setFalse, toggle];
 }
