f324c3615f561e8d808d75fad0f6cef422bc40e8
Valentine Yurchenko <45362610+VelHRH@users.noreply.github.com>
2022-12-11 21:07:56 +0300

    Registration layout improved

--- pages/registration.tsx
+++ pages/registration.tsx
@@ -9,15 +9,16 @@ const registration = () => {
    <CreateQueueHeading>
     Реєстрація
    </CreateQueueHeading>
-   <div className="flex flex-col w-10/12 md:w-1/3">
-     <form className="mt-9">
-       <Input placeholder="Email" />
-
-       <Input type="password" placeholder="Придумайте пароль" />
-
-       <div className='-mt-5'><Input type="password" placeholder="Повторіть пароль" /></div>
-
-       <Input placeholder="Ваше ім'я" />
+   <div className="flex flex-col w-10/12 md:w-2/3">
+     <form className="mt-9 justify-between flex flex-col md:flex-row">
+      <div className='flex flex-col md:w-[45%]'>
+        <Input placeholder="Email" />
+        <Input placeholder="Ваше ім'я" />
+       </div>
+       <div className='flex flex-col md:w-[45%]'>
+        <Input type="password" placeholder="Придумайте пароль" />
+        <Input type="password" placeholder="Повторіть пароль" />
+       </div>
      </form>
     <Button
      color="green"
