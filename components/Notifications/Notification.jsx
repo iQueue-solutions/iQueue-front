976791a639e543439b723afff1760019f06b2b7a
Valentine Yurchenko <45362610+VelHRH@users.noreply.github.com>
2022-12-12 19:26:30 +0300

    Notifications

--- components/Notifications/Notification.jsx
+++ components/Notifications/Notification.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import { ArrowRightIcon, XIcon, CheckIcon } from "@heroicons/react/outline";
 
-export const Notification = () => {
+export const Notification = ({from, to, user, queueName}) => {
   return (
-    <div className='p-2 bg-green-200 border-2 w-full border-green-600 rounded-xl mt-3'>d</div>
+    <div className='p-3 bg-green-200 border-2 w-full border-green-600 rounded-xl mt-3 text-slate-900 flex justify-between'>
+      <div className='flex-col w-full'>
+      <div className='flex font-semibold text-3xl items-center'>
+        <div className='text-slate-500'>{from}</div>
+        <ArrowRightIcon className='w-8 mx-2' />
+        {to}
+      </div>
+      <div className='flex text-xl text-slate-500 mt-1 flex-wrap'>
+        Від <span className='font-bold mx-2 text-slate-900'>{user}</span> у <span className='font-bold mx-2 text-slate-900'>{queueName}</span>
+      </div>
+      </div>
+      <div className='flex'>
+        <button>
+          <CheckIcon className='w-7 hover:text-green-700' />
+        </button>
+        <button>
+          <XIcon className='w-7 ml-2 hover:text-red-600' />
+        </button>
+      </div>
+    </div>
   )
 }
--- pages/notifications.js
+++ pages/notifications.js
@@ -6,9 +6,9 @@ const notifications = () => {
   return (
     <div className='w-10/12 md:w-1/2 flex flex-col ml-[50%] translate-x-[-50%]'>
       <Heading>Запити на заміну</Heading>
-      <div className='mt-5'>
-        <Notification />
-        <Notification />
+      <div className='mt-5 w-full'>
+        <Notification from={10} to={7} user="Спектров" queueName="ПарП" />
+        <Notification from={22} to={15} user="Спектров" queueName="ПарПППППППППППП" />
       </div>
     </div>
   )
