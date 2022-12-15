5256633afdcde7d96e3bca087696c9457c8535e6
Umbrien <kaluhin.serhii@nure.ua>
2022-12-15 03:59:57 +0200

    profile

--- components/Button.tsx
+++ components/Button.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "../styles/Button.module.css";
 
 interface ButtonProps {
-  color: 'orange' | 'purple';
+  color: 'orange' | 'purple' | 'red';
   variant: 'solid' | 'outline';
   onClick?: () => void;
   icon?: React.ReactNode;
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,36 +1,42 @@
 import {useContext} from "react";
 import {LayoutContext} from "../components/Layout/Layout";
-import {dehydrate, QueryClient, useQuery} from "@tanstack/react-query";
-import {getUsers} from "../fetchers/users";
 import {ProfileImage} from "../components/Profile/ProfileImage"
 import {ProfileInfo} from "../components/Profile/ProfileInfo"
 import { Button } from "../components/Button";
-
-export async function getServerSideProps() {
-  const queryClient = new QueryClient();
-  await queryClient.prefetchQuery(['users'], getUsers);
-
- return {
-  props: {
-    dehydratedState: dehydrate(queryClient),
-  },
- };
-}
+import {useRouter} from "next/router";
+import Link from "next/link";
 
 const Profile = () => {
-  const {user: contextUser, setUser} = useContext(LayoutContext);
+  const {user, setUser} = useContext(LayoutContext);
 
-  const {data} = useQuery({queryKey: ['users'], queryFn: getUsers});
+  const router = useRouter();
+
+  function logout() {
+    localStorage.removeItem('iQueue-jwt');
+    localStorage.removeItem('iQueue-user');
+    setUser(undefined);
+    router.push('/login');
+  }
+
   return (
     <>
-      <div className="flex flex-col mx-auto w-10/12 md:w-1/3 items-center mt-5">
-        <ProfileImage img={`https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png`} />
-        <ProfileInfo name={`Denys Spektrov`} email={`denys.spektrov@nure.ua`} />
-        <div className="flex w-1/2 mt-5 justify-around">
-          <Button color="purple" variant="outline">Вийти</Button>
-          <Button color="red" variant="outline">Видалити</Button>
+      {user && (
+        <div className="flex flex-col mx-auto w-10/12 md:w-1/3 items-center mt-5">
+          <ProfileImage img={`https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png`} />
+          <ProfileInfo name={`${user.firstName} ${user.lastName}`} email={user.email} />
+          <div className="flex w-1/2 mt-5 justify-around">
+            <Button color="purple" variant="outline" onClick={logout}>Вийти</Button>
+            <Button color="red" variant="outline">Видалити</Button>
+          </div>
         </div>
-      </div>
+      )}
+      {!user && (
+        <div>
+          <Link href={'/login'}>
+            <a>Вхід</a>
+          </Link>
+        </div>
+      )}
     </>
   );
 }
