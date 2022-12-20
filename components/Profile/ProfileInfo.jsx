04baa33e47498b5d41aaa1358a86b5f5e7d99fe4
Valentine Yurchenko <45362610+VelHRH@users.noreply.github.com>
2022-12-20 15:44:57 +0300

    Profile layout improved

--- components/Profile/ProfileInfo.jsx
+++ components/Profile/ProfileInfo.jsx
@@ -6,14 +6,14 @@ export const ProfileInfo = ({name, email, isEdit}) => {
     <>
       {!isEdit ?
       <>
-        <div className='text-2xl font-bold text-violet-800 mt-2'>{name}</div>
-        <div className='font-semibold'>{email}</div>
+        <div className='text-3xl font-bold text-violet-800 mt-2'>{name}</div>
+        <div className='font-semibold text-xl'>{email}</div>
       </> :
-      <div className='mt-5'>
+      <div className='mt-5 w-2/3 md:w-1/3 -mb-5'>
         <Input value={name} />
         <Input value={email} />
-        <Input placeholder='Введіть старий пароль' />
-        <Input placeholder='Введіть новий пароль' />
+        <Input type='password' placeholder='Введіть старий пароль' />
+        <Input type='password' placeholder='Введіть новий пароль' />
       </div>}
     </>
   )
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -22,15 +22,15 @@ const Profile = () => {
   return (
     <>
       {user && (
-        <div className="flex flex-col mx-auto w-10/12 md:w-1/3 items-center mt-5">
+        <div className="flex flex-col mx-auto w-full items-center mt-5">
           <ProfileImage img={`https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png`} />
           <ProfileInfo name={`${user.firstName} ${user.lastName}`} email={user.email} isEdit={isEdit} setIsEdit={setIsEdit} />
           {!isEdit ?
-          <div className="flex w-1/2 mt-5 justify-around">
+          <div className="flex w-1/2 md:w-1/3 mt-7 justify-around">
             <Button color="purple" variant="solid" onClick={logout}>Вийти</Button>
             <Button color="purple" variant="outline" onClick={() => setIsEdit(true)}>Редагувати</Button>
           </div> :
-          <div className="flex w-1/2 mt-5 justify-around">
+          <div className="flex w-1/3 mt-5 justify-around">
             <Button color="green" variant="solid" onClick={() => setIsEdit(false)}>Зберегти</Button>
           </div>
           }
